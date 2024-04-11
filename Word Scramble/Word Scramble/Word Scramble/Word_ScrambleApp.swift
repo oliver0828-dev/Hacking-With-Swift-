b99a17e3bfcbcd66369ecbaf67247662a9df50de
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2024-04-11 18:38:25 +0900

    Add files via upload

---
+++ Word Scramble/Word Scramble/Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Oliver Park on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
