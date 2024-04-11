2bd07fa5e43ab249379700e3f7b54f0369034ff4
Oliver Park <96828703+oliver0828-dev@users.noreply.github.com>
2024-04-11 18:12:11 +0900

    Add files via upload

---
+++ WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Oliver Park on 3/20/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
